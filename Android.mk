faf3b3c5131c30f8465676e9737936e5e9227e3f
gtpitch <bmontagu@gmail.com>
2018-01-21 00:27:51 -0500

    Snap: update package name
    Signed-off-by: gtpitch <bmontagu@gmail.com>

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ LOCAL_AAPT_FLAGS := \
         --version-name "$(version_name_package)" \
         --version-code $(version_code_package) \
 
-LOCAL_PACKAGE_NAME := SnapdragonCamera2
+LOCAL_PACKAGE_NAME := SnapdragonCamera
 
 #LOCAL_SDK_VERSION := current
 LOCAL_RENDERSCRIPT_TARGET_API := 23
